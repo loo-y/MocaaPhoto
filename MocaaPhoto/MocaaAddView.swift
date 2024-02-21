ba2e5e4ce6cc7d3944526c9e3fc63531f967b4c8
loo-y <loo-y@outlook.com>
2024-02-21 22:01:46 +0800

    WIP: add image to view

--- MocaaPhoto/MocaaAddView.swift
+++ MocaaPhoto/MocaaAddView.swift
@@ -14,6 +14,58 @@ import PhotosUI
 final class PhotoPickerViewModel: ObservableObject{
     
     @Published private(set) var selectedImage: NSImage? = nil
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
+    
+    @Published private(set) var selectedImages: [NSImage] = []
+    @Published var imageSelections: [PhotosPickerItem] = [] {
+        didSet {
+            setImages(from: imageSelections)
+        }
+    }
+    
+    private func setImage(from selection: PhotosPickerItem?){
+        guard let selection else { return }
+        
+        Task {
+//            if let data = try? await selection.loadTransferable(type: Data.self) {
+//                if let uiImage = NSImage(data: data){
+//                    selectedImage = uiImage
+//                    return;
+//                }
+//            }
+            
+            do {
+                let data = try? await selection.loadTransferable(type: Data.self)
+                guard let data, let uiImage = NSImage(data: data) else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                selectedImage = uiImage
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    private func setImages(from selections: [PhotosPickerItem]?){
+        guard let selections else { return }
+        
+        Task {
+            var images: [NSImage] = []
+            for selection in selections {
+                if let data = try? await selection.loadTransferable(type: Data.self) {
+                    if let uiImage = NSImage(data: data){
+                        selectedImage = uiImage
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct MocaaAddView: View {
@@ -21,15 +73,24 @@ struct MocaaAddView: View {
     @StateObject private var viewModel = PhotoPickerViewModel()
     
     var body: some View {
-        VStack(alignment: .center, spacing: 0) {
+        VStack(alignment: .center, spacing: 40) {
             Text("hello MocaaPhoto")
             
-            VStack(content: {
-                /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
-            })
-            .padding(.all)
+            if let image = viewModel.selectedImage {
+                Image(nsImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(10)
+            }
+            
+            PhotosPicker(selection: $viewModel.imageSelection, matching: .images) {
+                Text("Pick a Photo")
+                    .foregroundColor(.blue)
+            }
         }
         .padding(.all, 10.0)
+        .frame(minWidth: 600, minHeight: 400)
     }
 }
 
